b262acc3b3bb8174cc71b25dea9d68e8a76ca80a
Faisal Mahmud <62201274+Faisal778@users.noreply.github.com>
2021-09-04 01:26:54 +0600

    Update Random_month_generator.java

--- Random_month_generator.java
+++ Random_month_generator.java
@@ -1,43 +1,42 @@
 public class Random_month_generator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int number = (int)(Math.random()*((11-0)+1));
 		switch (number) {
-		case 1:
+		case 0:
 			System.out.println("january");
 			break;
-		case 2: 
+		case 1: 
 			System.out.println("February");
 			break;
-		case 3: 
+		case 2: 
 			System.out.println("March");
 			break;
-		case 4:
+		case 3:
 			System.out.println("April");
 			break;
-		case 5:
+		case 4:
 			System.out.println("May");
 			break;
-		case 6:
+		case 5:
 			System.out.println("June");
 			break;
-		case 7:
+		case 6:
 			System.out.println("July");
 			break;
-		case 8:
+		case 7:
 			System.out.println("August");
 			break;
-		case 9: 
+		case 8: 
 			System.out.println("September");
 			break;
-		case 10:
+		case 9:
 			System.out.println("October");
 			break;
-		case 11:
+		case 10:
 			System.out.println("November");
 			break;
-		case 12:
+		case 11:
 			System.out.println("December");
 			break;
 			
@@ -45,6 +44,7 @@ public class Random_month_generator {
 	}
 
 }
+
 
 /* (Random month) Write a program that randomly generates an integer between 1 and 12 and displays
 the English month name January, February, ..., December for the number 1, 2, ..., 12, accordingly.  */
